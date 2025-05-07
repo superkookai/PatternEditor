1f2ce2e1302d05c2269a45ad6c555fbf67663777
superkookai <weerawut@live.com>
2025-05-07 13:56:18 +0700

    Initial Commit

---
+++ PatternEditor/PatternEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatternEditorApp.swift
+//  PatternEditor
+//
+//  Created by Weerawut Chaiyasomboon on 07/05/2568.
+//
+
+import SwiftUI
+
+@main
+struct PatternEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
